00b3d3a359fe797321115f0554b45f6f87824772
SelormBruce <developerin2020@gmail.com>
2019-01-10 07:30:14 +0000

    projects design

--- src/app/Model/home/home.component.ts
+++ src/app/Model/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as M from 'src/assets/materialize/js/materialize.min.js';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
-
+  options ={
+    fullWidth: true,
+    indicators: true
+  };
   ngOnInit() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, this.options);
   }
-
 }
