46ef986c524b14edbd33e031a6201f071f5b645f
SelormBruce <developerin2020@gmail.com>
2019-01-10 06:29:22 +0000

    footer dedsign

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule, MatCheckboxModule, MatToolbarModule, 
-  MatIconModule, MatFormFieldModule, MatSelectModule, MatCardModule
+  MatIconModule, MatFormFieldModule, MatSelectModule, MatCardModule,
+  MatExpansionModule
  } from '@angular/material';
 
 @NgModule({
   declarations: [],
   imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule,
-    CommonModule, MatFormFieldModule, MatSelectModule, MatCardModule
+    CommonModule, MatFormFieldModule, MatSelectModule, MatCardModule,MatExpansionModule
   ],
   exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, 
-    MatIconModule, MatFormFieldModule, MatSelectModule, MatCardModule]
+    MatIconModule, MatFormFieldModule, MatSelectModule, MatCardModule,MatExpansionModule]
 })
 export class MaterialModule { }
